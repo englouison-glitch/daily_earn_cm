9b7883f542ba506bb13d907a2c8c89d2ffcda63a
englouison-glitch <englouison@gmail.com>
2026-08-05 00:50:01 +0100

    Create TasksScreen widget with necessary imports

---
+++ lib/tasks_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'localization/app_localization.dart';
+
+class TasksScreen extends StatefulWidget {
+  const TasksScreen({super.key});
+
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _
+
